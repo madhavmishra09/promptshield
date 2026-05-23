26877e3361fd888cc46214287ea05b3c0de3d8fb
Madhav Mishra <madhavmishra083@gmail.com>
2026-05-23 19:54:24 +0530

    feat: logging added

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ const express = require('express');
 const cors = require('cors');
 
 const chatRoute= require('./routes/chat');
+const logRoute=require('./routes/logs');
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/chat', chatRoute);
+app.use('/api/logs', logRoute);
 app.listen(5000,()=>{
     console.log('Server is running on port 5000');
 });
